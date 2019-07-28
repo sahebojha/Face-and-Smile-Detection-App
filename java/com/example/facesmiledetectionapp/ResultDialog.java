d5824b12e422cb50069e655286e93d875e9e7bf9
Saheb ojha <46969653+sahebojha@users.noreply.github.com>
2019-07-29 00:06:14 +0530

    Add files via upload

---
+++ java/com/example/facesmiledetectionapp/ResultDialog.java
@@ -0,0 +1,40 @@
+package com.example.facesmiledetectionapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultDialog extends DialogFragment {
+
+    private Button okButton;
+    private TextView resulttv;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_resultdialog,container,false);
+        String resultText = "";
+        okButton = view.findViewById(R.id.result_ok_button);
+        resulttv = view.findViewById(R.id.result_text_view);
+
+        Bundle bundle = getArguments();
+        resultText = bundle.getString(Facedetection.RESULT_TEXT);
+        resulttv.setText(resultText);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
